518b706223edf8ee0eb14f4b40cdcd038cee5fe5
Navaneeth Nivol <navaneethsai1999@gmail.com>
2020-09-12 19:06:02 +0530

    Board APIs Added and auth controller file name changed to authCtrl.js  Changes to be committed: 	modified:   APIs/Routes/index.js 	renamed:    APIs/controllers/auth.js -> APIs/controllers/authCtrl.js 	new file:   APIs/controllers/boardCtrl.js

--- APIs/Routes/index.js
+++ APIs/Routes/index.js
@@ -3,18 +3,23 @@ var router = express.Router();
 
 
 // Controllers
-var auth = require('../controllers/auth');
+var authCtrl = require('../controllers/authCtrl');
+var boardCtrl = require('../controllers/boardCtrl');
 
 
 // Middlewares
-var auth_middleware = require('../middlewares/auth');
+var auth = require('../middlewares/auth');
 const { worker } = require('cluster');
 
 //------------------------------------ Login/Register APIs ------------------------------------//
 
-router.post('/login', auth.login);
-router.post('/register', auth.register);
+router.post('/login', authCtrl.login);
+router.post('/register', authCtrl.register);
 
+//------------------------------------ Board APIs ------------------------------------//
+
+router.get('/user/get/boards', boardCtrl.getUserBoards);
+router.post('/user/save/board', boardCtrl.saveUserBoardData);
 
 
 module.exports = router;
