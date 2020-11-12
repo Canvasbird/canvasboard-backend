eb74a25a6f756f55b468f889f67ac8d44633da47
unknown <akashmadduru@gmail.com>
2020-11-12 19:33:47 +0530

    using jwt_token to access folders

--- APIs/controllers/file/renameFile.js
+++ APIs/controllers/file/renameFile.js
@@ -1,16 +1,40 @@
 const { Files } = require("../../models/db");
-const { httpStatus200, httpStatus500 } = require("../../status/httpStatus")
+const { httpStatus200, httpStatus500 } = require("../../status/httpStatus");
+// const minio = require("../../config/minio");
 
-exports.renameFileAttributes = async (req, res) =>{
+exports.renameFileAttributes = async (req, res) => {
+  if (req.body.is_modified) {
     try {
-        await Files.findByIdAndUpdate(req.body.file_id, {
-            $set:{
-                file_name:req.body.file_name,
-                file_tag:req.body.file_tag
-            }
-        })
-        res.status(200).json(httpStatus200(null, "File Renamed"))
+      await Files.findByIdAndUpdate(req.body.file_id, {
+        $set: {
+          file_name: req.body.file_name,
+          file_tag: req.body.file_tag,
+          last_accessed_on: Date.now(),
+          last_modified_on: Date.now(),
+          // file_url:req.file.originalname
+        },
+      });
+      //   await minio.minioClient.putObject(
+      //     "files",
+      //     req.file.originalname,
+      //     req.file.buffer
+      //   );
+      res.status(200).json(httpStatus200(null, "File Renamed"));
     } catch (error) {
-        if(error) res.status(500).json(httpStatus500(error))
+      if (error) res.status(500).json(httpStatus500(error));
     }
-}
+  } else {
+    try {
+      await Files.findByIdAndUpdate(req.body.file_id, {
+        $set: {
+          file_name: req.body.file_name,
+          file_tag: req.body.file_tag,
+          last_accessed_on: Date.now(),
+        },
+      });
+      res.status(200).json(httpStatus200(null, "File Renamed"));
+    } catch (error) {
+      if (error) res.status(500).json(httpStatus500(error));
+    }
+  }
+};
--- APIs/controllers/folder/addFolder.js
+++ APIs/controllers/folder/addFolder.js
@@ -1,31 +1,32 @@
-const { httpStatus200, httpStatus500} = require('../../status/httpStatus')
-const {Users, Folders} = require('../../models/db')
+const { httpStatus200, httpStatus500 } = require("../../status/httpStatus");
+const { Users, Folders } = require("../../models/db");
+const jwt = require("jsonwebtoken");
 
 exports.createFolder = async (req, res) => {
+  try {
+    var userId = jwt.verify(
+      req.get("X-AUTH-TOKEN"),
+      process.env.JWT_SECRET_KEY
+    );
+
+    const folder = await new Folders({
+      folder_name: req.body.folder_name,
+      folder_title: req.body.folder_title,
+      folder_tag: req.body.folder_tag,
+      is_nested_folder: req.body.is_nested_folder,
+    });
+
     try {
-
-        const folder = await new Folders({
-            folder_name:req.body.folder_name,
-            folder_title:req.body.folder_title,
-            is_nested_folder:req.body.is_nested_folder
-        })
-
-        try {
-
-            const { _id } = await folder.save()  
-            const user = await Users.findById(req.body.user_id)
-            const message = await user.addFolder(_id);
-            res.status(200).json(httpStatus200(folder, message))
-
-        } catch (error) {
-            console.log(error)
-            if(error) res.status(500).json(httpStatus500(error))
-        }
-
+      const { _id } = await folder.save();
+      const user = await Users.findById(userId.user_id);
+      const message = await user.addFolder(_id);
+      res.status(200).json(httpStatus200(folder, message));
     } catch (error) {
-        
-        console.log(error)
-        if(error) res.status(500).json(httpStatus500(error))
+      console.log(error);
+      if (error) res.status(500).json(httpStatus500(error));
     }
-
-}
+  } catch (error) {
+    console.log(error);
+    if (error) res.status(500).json(httpStatus500(error));
+  }
+};
