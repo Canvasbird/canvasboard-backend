9fc3177420e6ebb4c9a21730cef93fef9ee97e2e
Akash Madduru <33013583+akashmrc98@users.noreply.github.com>
2020-11-21 14:19:21 +0000

    mend (#23)
    fixing file issue

--- APIs/Routes/folder.js
+++ APIs/Routes/folder.js
@@ -4,27 +4,18 @@ var router = express.Router();
 const { isAuthenticated } = require("../middlewares/auth");
 
 const { createFolder } = require("../controllers/folder/addFolder");
-const {
-  renameFolderAttributes,
-} = require("../controllers/folder/renameFolder");
+const { renameFolderAttributes } = require("../controllers/folder/renameFolder");
 const { removeFolder } = require("../controllers/folder/removeFolder");
 const { viewUserFolders } = require("../controllers/folder/viewFolders");
-
 const { createChildFolder } = require("../controllers/folder/addChildFolder");
-const {
-  removeChildFolder,
-} = require("../controllers/folder/removeChildFolder");
+const { removeChildFolder } = require("../controllers/folder/removeChildFolder");
 const { viewChildFolders } = require("../controllers/folder/viewChildFolders");
+
+// * Folder Routes
 
 router.post("/user/create-folder", isAuthenticated, createFolder);
 router.post("/user/rename-folder", isAuthenticated, renameFolderAttributes);
-
 router.delete("/user/remove-folder", isAuthenticated, removeFolder);
-<<<<<<< HEAD
-=======
-router.get("/user/view-folders", isAuthenticated, viewUserFolders);
->>>>>>> f01c19f16df7518d668f46e5460dd5a538c59bd7
-
 router.get("/user/view-folders", isAuthenticated, viewUserFolders);
 router.post("/user/create-nested-folder", createChildFolder);
 router.delete("/user/remove-nested-folder", isAuthenticated, removeChildFolder);
--- APIs/controllers/file/renameFile.js
+++ APIs/controllers/file/renameFile.js
@@ -1,35 +1,22 @@
 const { Files } = require("../../models/db");
 const { httpStatus200, httpStatus500 } = require("../../status/httpStatus");
-// const minio = require("../../config/minio");
 
 exports.renameFileAttributes = async (req, res) => {
   if (req.body.is_modified) {
     try {
-<<<<<<< HEAD
       const changedContent = await Files.findByIdAndUpdate(req.body.file_id, {
-=======
-      await Files.findByIdAndUpdate(req.body.file_id, {
->>>>>>> f01c19f16df7518d668f46e5460dd5a538c59bd7
         $set: {
           file_name: req.body.file_name,
           file_tag: req.body.file_tag,
           last_accessed_on: Date.now(),
           last_modified_on: Date.now(),
-          // file_url:req.file.originalname
+          data:req.body.data
         },
       });
-      //   await minio.minioClient.putObject(
-      //     "files",
-      //     req.file.originalname,
-      //     req.file.buffer
-      //   );
-<<<<<<< HEAD
-
-      if (changedContent !== null) {
+      if (changedContent !== null) 
         res.status(200).json(httpStatus200(null, "File Renamed"));
-      } else {
+       else 
         res.status(500).json(httpStatus500("Invalid File Id"));
-      }
     } catch (error) {
       if (error) res.status(500).json(httpStatus500(error));
     }
@@ -42,32 +29,12 @@ exports.renameFileAttributes = async (req, res) => {
           last_accessed_on: Date.now(),
         },
       });
-      if (changedContent !== null) {
+      if (changedContent !== null) 
         res.status(200).json(httpStatus200(null, "File Renamed"));
-      } else {
+      else 
         res.status(500).json(httpStatus500("Invalid File Id"));
-      }
     } catch (error) {
       if (error) res.status(500).json(httpStatus500(error));
     }
-=======
-      res.status(200).json(httpStatus200(null, "File Renamed"));
-    } catch (error) {
-      if (error) res.status(500).json(httpStatus500(error));
-    }
-  } else {
-    try {
-      await Files.findByIdAndUpdate(req.body.file_id, {
-        $set: {
-          file_name: req.body.file_name,
-          file_tag: req.body.file_tag,
-          last_accessed_on: Date.now(),
-        },
-      });
-      res.status(200).json(httpStatus200(null, "File Renamed"));
-    } catch (error) {
-      if (error) res.status(500).json(httpStatus500(error));
-    }
->>>>>>> f01c19f16df7518d668f46e5460dd5a538c59bd7
   }
 };
