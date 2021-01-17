dc7703c1928e2d0f2b06ac4906abd265bf0d129a
AsishRaju <41658324+AsishRaju@users.noreply.github.com>
2021-01-17 19:59:26 +0530

    fixed file id

--- APIs/controllers/file/createFile.js
+++ APIs/controllers/file/createFile.js
@@ -7,14 +7,19 @@ exports.createFile = async (req, res) => {
     const file = await new Files({
       file_name: req.body.file_name,
       file_tag: req.body.file_tag,
-      data:req.body.data
+      data: req.body.data
     });
     await file.save();
 
     try {
       const rootFolder = await Folders.findById(req.body.folder_id);
       const message = await rootFolder.addChildFile(file._id);
-      res.status(200).json(httpStatus200(null, message));
+      // res.status(200).json(httpStatus200(null, message));
+      return res.status(200).json({
+        success: true,
+        message: message,
+        file: file
+      });
     } catch (error) {
       if (error) res.status(500).json(httpStatus500(error));
     }
