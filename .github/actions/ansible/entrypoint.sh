f3fd5877ff08840523192d2c41c59b7e57250571
goliakshay357 <goliakshay357@gmail.com>
2021-04-28 17:16:12 +0530

    Update entrypoint.sh

--- .github/actions/ansible/entrypoint.sh
+++ .github/actions/ansible/entrypoint.sh
@@ -15,4 +15,4 @@ echo "ansible_become_pass=$SSH_PASSWORD" >> /hosts
 
 echo "Entering the ansible using ansible-playbook"
 
-ansible-playbook ansible/playbook.yml --user ubuntu
+ansible-playbook ansible/playbook.yml --user $SSH_USER
