8465468988903194761b4b4162f126063f95b090
nick-w-nick <43578531+nick-w-nick@users.noreply.github.com>
2021-10-13 14:03:33 -0400

    miscellaneous reorganization + formatting updates

--- APIs/app.js
+++ APIs/app.js
@@ -1,54 +1,42 @@
-const express = require("express");
+const express = require('express');
 const cors = require('cors');
-const morgan = require('morgan')
-const Sentry = require("@sentry/node");
-const Tracing = require("@sentry/tracing");
+const morgan = require('morgan');
+const Sentry = require('@sentry/node');
+const Tracing = require('@sentry/tracing');
 
 const config = require('./config/config');
 
 const router = require('./Routes/index');
 const folderRoutes = require('./Routes/folder');
-const fileRoutes = require('./Routes/file')
+const fileRoutes = require('./Routes/file');
 
 const app = express();
+
 // ------------------ Sentry ----------------------
 Sentry.init({
-  dsn: "https://7905f16e0ef747ee970d420eefa65296@sentry.canvasboard.live/8",
-
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
-  integrations: [
-      new Sentry.Integrations.Http({ tracing: true }),
-      new Tracing.Integrations.Express({
-          app,
-      }),
-  ],
-  tracesSampleRate: 1.0,
+    dsn: "https://7905f16e0ef747ee970d420eefa65296@sentry.canvasboard.live/8",
+    
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    integrations: [
+        new Sentry.Integrations.Http({ tracing: true }),
+        new Tracing.Integrations.Express({
+            app,
+        }),
+    ],
+    tracesSampleRate: 1.0,
 });
 
 app.use(Sentry.Handlers.requestHandler());
 app.use(Sentry.Handlers.tracingHandler());
 // -------------------------------------------------
-app.use(morgan('dev'))
+
+app.use(morgan('dev'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
-
-
-
-app.get("/", function (req, res) {
-  res.status(200).json({
-    success: true,
-    message: "Welcome to the Canvasboard APIs"
-  });
-});
-
-
-app.use('/api/v1', router);
-app.use('/api/v1', folderRoutes)
-app.use('/api/v1', fileRoutes)
 
 app.listen(config.app.port, (err) => {
     if (err) {
@@ -56,3 +44,14 @@ app.listen(config.app.port, (err) => {
     }
     console.log(`\nServer successfully started on PORT: ${config.app.port} ✔️`);
 });
+
+app.get('/', (req, res) => {
+    return res.status(200).send({
+        success: true,
+        message: 'Welcome to the Canvasboard APIs'
+    });
+});
+
+app.use('/api/v1', router);
+app.use('/api/v1', folderRoutes);
+app.use('/api/v1', fileRoutes);
